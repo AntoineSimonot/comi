97f320f8ee43ba3f2760ef324ae167749bc037ff
KoHorizon <73652886+KoHorizon@users.noreply.github.com>
2021-05-19 21:02:18 +0200

    Temporary fix to Mail Section bug

--- src/components/Mail/TabsMailView.js
+++ src/components/Mail/TabsMailView.js
@@ -12,7 +12,9 @@ const TabsMailView = (props) => {
     const [data, setData] = useState ([])
     const [fetch, setFetch] = useState (true)
     const [url, setUrl] = useState (["unread"])
-    const [projet, setprojet] = useState (false)
+    const [projet, setProjet] = useState ([])
+    const [change, setChange] = useState (false)
+
     let { ChangeURL } = props
 
     useEffect(() => {
@@ -25,18 +27,46 @@ const TabsMailView = (props) => {
           axios.get(`http://localhost:3000/messages/${url}`).then(function (response) {
           setData(response.data)
           setFetch(false)
+          setChange(false)
         })
       } 
-
+      // console.log(data);
     }, [data,ChangeURL,fetch, url, projet])
 
 
     const changeVariableProject = (urlName) => {
-      setUrl(urlName)
-      setFetch(true)
+      axios.get(`http://localhost:3000/messages/section/${urlName}`).then(function (response) {
+        setData(response.data)
+        console.log(response.data);
+      })  
+      setChange(true)
     } 
     
-    const mailCreation = () => {
+
+    const mailOfSection = () => {
+      const liste = []
+
+      for (const message of data) {
+        liste.push(
+        <div>
+          <input type="checkbox" class="check" />
+          <div class="sender-img">
+            <div class="profil-img"></div>
+            <div class="mail-notification"></div>
+          </div>
+          <p class="mail-title">{message.title}</p>
+          <p class="mail-content">
+          {message.content}
+          </p>
+        </div>
+        )
+      }
+
+    return liste
+    }
+
+
+    const allMail = () => {
 
       const liste = []
 
@@ -61,8 +91,9 @@ const TabsMailView = (props) => {
         for (const projet of data) {
           liste.push(
           <div>
-            <p onClick={() => changeVariableProject(`unread/${projet.name}`)} class="mail-title">{projet.name}</p>
+            <p onClick={() => changeVariableProject(`${projet.name}`)} class="mail-title">{projet.name}</p>
           </div>
+          
           )
         }
       }
@@ -76,7 +107,7 @@ const TabsMailView = (props) => {
       <div className="content-message">
         <span>Date</span>
         <div className="mail">
-        {mailCreation()}
+        {change == false ? allMail() : mailOfSection() }
         </div>
       </div>
     </div>
